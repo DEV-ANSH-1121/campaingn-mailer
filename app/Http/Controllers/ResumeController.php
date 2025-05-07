c7d547074aed081e374e0a35325a90214e69a282
Saurabh Suman <devsaurabhsuman@gmail.com>
2025-05-07 14:10:15 +0530

    Resume Builder

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -10,14 +10,7 @@ class ResumeController extends Controller
 {
     public function generatePdf(Request $request)
     {
-        // return view('ansh-resume');
-        // return view('rahul-resume');
-        // return view('nikku-resume');
-        $view = 'rahul-resume';
-        if ($request->has('resume')) {
-            $view = $request->resume;
-        }
-
+        $view = $request->get('resume', 'ansh-resume');
         $pdf = Pdf::loadView($view); // Load the Blade view
         return $pdf->download($view . '.pdf'); // Download as PDF
     }
